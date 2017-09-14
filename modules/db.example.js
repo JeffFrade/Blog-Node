96157ac51769eb35dd53087129d628d9aaae1415
Jefferson Frade <jefferrson.frade@gmail.com>
2017-09-14 14:43:49 -0300

    Corrindo Bugs

---
+++ modules/db.example.js
@@ -0,0 +1,12 @@
+//Renomeie Este Arquivo Para db.js, Fazendo as Devidas Alterações de Parâmetros
+module.exports = function() {
+	var mysql = require('mysql');
+	var conn = mysql.createConnection({
+		host: "host",
+		user: "usuário",
+		password: "senha",
+		database: "blog_node"
+	});
+
+	conn.connect();
+};
