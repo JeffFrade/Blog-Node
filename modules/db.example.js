14930fea170e1d586f896efb8710cbb0d66cacf2
Jefferson Frade <jefferrson.frade@gmail.com>
2017-09-17 15:28:30 -0300

    Corrigindo Acesso ao Banco de Dados

--- modules/db.example.js
+++ modules/db.example.js
@@ -1,12 +1,13 @@
 //Renomeie Este Arquivo Para db.js, Fazendo as Devidas Alterações de Parâmetros
-module.exports = function() {
-	var mysql = require('mysql');
-	var conn = mysql.createConnection({
+var mysql = require('mysql');
+
+var con = function() {
+	return mysql.createConnection({
 		host: "host",
 		user: "usuário",
 		password: "senha",
 		database: "blog_node"
 	});
-
-	conn.connect();
 };
+
+module.exports = con;
